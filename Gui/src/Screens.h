c1b56a17a145518737811199153581d62a73f3b3
Wakate <moody-8@live.com>
2023-03-11 02:38:21 +0400

    Add program logo

--- Gui/src/Screens.h
+++ Gui/src/Screens.h
@@ -8,6 +8,9 @@
 #include <vector>
 #include <tuple>
 
+#define DIP_SIZE(width, height, window) wxWindow::FromDIP(wxSize(width, height), window)
+#define DIP_POINT(x, y, window) wxWindow::FromDIP(wxPoint(x, y), window)
+
 typedef wxPanel* Screen;
 using std::vector;
 using std::tuple;
--- Gui/src/Screens/AdminRegister.cpp
+++ Gui/src/Screens/AdminRegister.cpp
@@ -18,16 +18,34 @@ Screen setupAdminRegister(wxWindow* parent)
     //Create screen parameters
     Screen screen = new wxPanel(parent);
     screen->SetSize(parent->GetSize());
-    screen->Show(false);
+    screen->Show(true);
+    screen->SetBackgroundColour(wxColor("#FFFFFF"));
 
     //Add screen pointer to list
     screensReference.push_back(make_tuple(screen, ADMIN_REGISTER));
 
     //Implement screen content
-    wxStaticText* text = new wxStaticText(screen, wxID_ANY, "Admin Register screen", wxDefaultPosition, wxDefaultSize, wxALIGN_RIGHT);
-    wxButton* button = new wxButton(screen, wxID_ANY, "Register Admin", wxPoint(700, 600));
+    wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+    screen->SetSizer(sizer);
 
-    //Bind controls with functions
+
+    wxPNGHandler* p = new wxPNGHandler();
+    wxImage::AddHandler(p);
+    wxStaticBitmap* bitmapImage = new wxStaticBitmap(screen, wxID_ANY, wxBitmap(wxImage("C:\\Users\\moody\\Desktop\\icon.png", wxBITMAP_TYPE_PNG).Rescale(350, 350, wxIMAGE_QUALITY_HIGH)));
+
+
+    wxStaticText* text = new wxStaticText(screen, wxID_ANY, "Admin Registration", wxDefaultPosition, wxDefaultSize);
+    text->SetPosition(DIP_POINT(489, 300, text));
+    text->SetFont(text->GetFont().Scale(2.0).MakeUnderlined());
+
+
+    wxButton* button = new wxButton(screen, wxID_ANY, "Register Admin");
+
+    //Bind controls with functions and add controls to sizer
     button->Bind(wxEVT_BUTTON, [](wxCommandEvent& evt) {RegisterAdmin(); });
+    sizer->Add(bitmapImage, 0, wxALIGN_CENTER);
+    sizer->Add(button, 0, wxALIGN_TOP | wxRIGHT);
+    
+    
     return screen;
 }
