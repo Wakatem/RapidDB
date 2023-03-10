87762fbb25b75ea5566d574526a27e99196ee253
Wakate <moody-8@live.com>
2023-03-11 00:41:58 +0400

    Fix window size not relative on different screens

--- Gui/src/MainFrame.cpp
+++ Gui/src/MainFrame.cpp
@@ -1,6 +1,7 @@
 #include "MainFrame.h"
 
-MainFrame::MainFrame(wxString& title) : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxSize(2000, 1200), wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
+MainFrame::MainFrame(wxString& title) : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
 {
-	CreateStatusBar();
+	//Set window size
+	SetSize(this->FromDIP(wxSize(1200, 700)));
 }
