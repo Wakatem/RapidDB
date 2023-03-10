3a83d2959d6dbbaf09f43ad703dcc6e5147c4551
Wakate <moody-8@live.com>
2023-03-11 00:43:03 +0400

    Center window

--- Gui/src/MainFrame.cpp
+++ Gui/src/MainFrame.cpp
@@ -2,6 +2,7 @@
 
 MainFrame::MainFrame(wxString& title) : wxFrame(nullptr, wxID_ANY, title, wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX))
 {
-	//Set window size
+	//Set window size and center it
 	SetSize(this->FromDIP(wxSize(1200, 700)));
+	Center();
 }
