c3ed9c11c350a32accbfe893a6f25cbda6516031
Wakate <moody-8@live.com>
2023-03-18 13:52:45 +0400

    (Append update to previous commit)

--- Logic/headers/Organization.h
+++ Logic/headers/Organization.h
@@ -4,12 +4,15 @@
 #include <vector>
 #include "Database.h"
 #include "User.h"
-
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/serialization.hpp>
 using namespace::std;
 
 class Organization
 {
+	friend class boost::serialization::access;
 private:
+
 	string name;
 	string country;
 	string timezone;
@@ -20,15 +23,30 @@ private:
 	vector <Database> databases;
 	//vector <Report> reports;
 
+	//serialization of non-POD data
+	template<class Archive>
+	void serialize(Archive& ar, const unsigned int version)
+	{
+		ar& name;
+		ar& country;
+		ar& passcode;
+		ar& privatekey;
+		ar& email;
+		ar& users;
+		ar& databases;
+	}
+
 public:
 	Organization(string orgName, string orgCountry, string timezone, string passcode, string email);
 	Organization();
+
 	string gerOrgName();
 	string getCountry();
 	string getTimezone();
 	string getPasscode();
 	string getPrivatekey();
 	string getEmail();
+
 	vector <User> getUsers();
 	vector <Database> getDatabases();
 	void setPrivatekey(string newPrivatekey);
